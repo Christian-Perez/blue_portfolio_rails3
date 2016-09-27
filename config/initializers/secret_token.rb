d67965b23929ad3834d225f375b3164be852a205
Christian Perez <chris.dillon92@gmail.com>
2016-09-27 14:42:52 -0700

    init repo, fresh new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioBluehost::Application.config.secret_token = '822309f8d233742eacc8af87d6fefd2be36c2998cc3bc4dc1d3b1c6e2107a894dc4ce12e374c02c06e97a8820232cd2a0e182fbd386409904153f3faadc700eb'
